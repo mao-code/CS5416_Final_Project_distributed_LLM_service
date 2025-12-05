df1297401bfc2ea65f0139578e3a72c9c41a7fe4
MaoXun <a22908352@gmail.com>
2025-12-04 20:52:40 -0500

    hotfix

--- client.sh
+++ client.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Sending test queries to Node0 with IP:$NODE_0_IP..."
-NODE_0_IP=$NODE_0_IP python -m distributed.client
+NODE_0_IP=$NODE_0_IP python client.py
+
+# Example Usage
+# NODE_0_IP=127.0.0.1:8000 \
+# ./cient.sh
